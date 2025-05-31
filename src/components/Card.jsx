662c687fd194fbce41a533e2e2c642e4d303d029
Sarthak Abraham <sarthakabraham@gmail.com>
2025-05-30 18:22:37 -0600

    Updated icons for grid section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import imageSource from "../assets/coding_1329016.png";
 
-function Card({ header, description }) {
+function Card({ header, description, image }) {
   return (
     <div className="p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
-      <img src={imageSource} className="w-20 h-20" />
+      <img src={image} className="w-20 h-20" />
       <h5 className="mt-4 mb-2 text-xl font-bold tracking-tight text-gray-800">
         {header}
       </h5>
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import Card from "./Card";
+
 function Grid() {
-    return(
-        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 p-2 md:p-6">
-            <Card header="Web Development" description="Take your business online with a highly responsive, attractive, and professional website."/>
-            <Card header="SEO" description="Boost your visibility with expert SEO strategies that drive traffic and grow your business."/>
-            <Card header="Social Media Optimization (SMO)" description="Enhance your brand presence and engagement across platforms with data-driven social media strategies that connect and convert."/>
-            <Card header="Social media ads" description="Enhance your brand presence and engagement across platforms with data-driven social media strategies that connect and convert."/>
-            <Card header="E-commerce Website" description="Take your business online with a highly responsive, attractive, and professional website."/>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-1 gap-3 md:grid-cols-2 p-2 md:p-6">
+      <Card
+        header="Web Development"
+        description="Take your business online with a highly responsive, attractive, and professional website."
+        image="./coding_1329016.png"
+      />
+      <Card
+        header="SEO"
+        image="./seo.png"
+        description="Boost your visibility with expert SEO strategies that drive traffic and grow your business."
+      />
+      <Card
+        image="social-media_1968777.png"
+        header="Social Media Optimization (SMO)"
+        description="Enhance your brand presence and engagement across platforms with data-driven social media strategies that connect and convert."
+      />
+      <Card
+        image="marketing_15245530.png"
+        header="Social media ads"
+        description="Enhance your brand presence and engagement across platforms with data-driven social media strategies that connect and convert."
+      />
+      <Card
+        image="bag_15245414.png"
+        header="E-commerce Website"
+        description="Take your business online with a highly responsive, attractive, and professional website."
+      />
+    </div>
+  );
 }
 
 export default Grid;
