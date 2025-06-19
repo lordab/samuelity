18ccb883da8d80ede4424ef0819e329375a62edc
Sarthak Abraham <sarthakabraham@gmail.com>
2025-06-18 18:40:48 -0600

    updated font color

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -7,10 +7,10 @@ function HeroComponent() {
   return (
     <div className="bg-[url(/hero_section_bg.jpg)] bg-no-repeat rounded-sm bg-cover bg-center bg-origin-padding md:w-full min-h-100 md:min-h-100">
       <div className="pt-2 flex flex-col sm:max-w-lg items-center pl-4 pr-6 ml-4 md:mx-auto">
-        <h1 className="font-extrabold text-lg font-inter text-gray-900 mt-2 sm:text-3xl sm:mt-4">
+        <h1 className="font-extrabold text-lg font-inter text-white mt-2 sm:text-3xl sm:mt-4">
           Smart Digital Solutions
         </h1>
-        <p className="font-medium text-sm text-gray-850 font-inter sm:text-lg mt-0.5 md:mt-2">
+        <p className="font-medium text-sm text-white font-inter sm:text-lg mt-0.5 md:mt-2">
           We design beautiful websites and get them seen with smart SEO. No
           jargon, no fluff—just real solutions that work. Build it. Rank it.
           Grow it.
